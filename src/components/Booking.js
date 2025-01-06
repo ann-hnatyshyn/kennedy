7c7ed9fde6b0ff23c5c5c7d8b7c3ff555c746cb4
Ann <a.m.hnatyshyn@gmail.com>
2025-01-06 09:36:12 -0400

    add button

--- src/components/Booking.js
+++ src/components/Booking.js
@@ -2,10 +2,14 @@ import React from "react";
 
 const Booking = () => {
   return (
-
-      <h1>I am the Booking component</h1>
-
-  );
+    <section className="newtweet">
+      <form method="post" action="/tweets" className="newtweet__form" onSubmit={(event) => event.preventDefault()}>
+        <textarea className="form__textarea" name="text" placeholder="What are you humming about?"></textarea>
+        <input type="submit" value="Tweet" className="form__input"/>
+        <span className="form__counter">140</span>
+      </form>
+    </section>
+    );
 };
 
 export default Booking;
--- src/components/Button.js
+++ src/components/Button.js
@@ -1,11 +1,11 @@
 import React from "react";
 
 const Button = () => {
-  return (
-
-      <h1>I am the button component</h1>
-
-  );
-};
+    return (
+      <button onClick={() => console.log("Button Clicked")}>
+        Click me!
+      </button>
+    );
+  }
 
 export default Button;
